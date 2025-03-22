bd9328867e5882ddbbcec0efe8603562180a29a1
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2025-03-22 23:54:25 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from mcp.server.fastmcp import FastMCP
+import wikipedia
+
+mcp = FastMCP(
+    "Wikipedia",
+    dependencies=["wikipedia"],
+)
+
+@mcp.tool()
+def search(query: str):
+    return wikipedia.search(query)
+
+@mcp.tool()
+def summary(query: str):
+    return wikipedia.summary(query)
+
+@mcp.tool()
+def page(query: str):
+    return wikipedia.page(query)
+
+@mcp.tool()
+def random():
+    return wikipedia.random()
+
+@mcp.tool()
+def set_lang(lang: str):
+    wikipedia.set_lang(lang)
+    return f"Language set to {lang}"
+
+if __name__ == "__main__":
+    mcp.run()
